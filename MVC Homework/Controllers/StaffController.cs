b8d319dba757a963330a7a0d2de59d559c989260
jmlindsey <jonathon.m.lindsey@gmail.com>
2018-02-23 17:43:14 -0600

    initial commit

---
+++ MVC Homework/Controllers/StaffController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Homework;
+
+namespace MVC_Homework.Controllers
+{
+    public class StaffController : Controller
+    {
+        private EfExampleEntities db = new EfExampleEntities();
+
+        // GET: Staffs
+        public ActionResult Index()
+        {
+            var staff = db.Staff.Include(s => s.Teacher);
+            return View(staff.ToList());
+        }
+
+        // GET: Staffs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Staff staff = db.Staff.Find(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            return View(staff);
+        }
+
+        // GET: Staffs/Create
+        public ActionResult Create()
+        {
+            ViewBag.Id = new SelectList(db.Teachers, "StaffId", "Specialty");
+            return View();
+        }
+
+        // POST: Staffs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Salary")] Staff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Staff.Add(staff);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Id = new SelectList(db.Teachers, "StaffId", "Specialty", staff.Id);
+            return View(staff);
+        }
+
+        // GET: Staffs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Staff staff = db.Staff.Find(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Id = new SelectList(db.Teachers, "StaffId", "Specialty", staff.Id);
+            return View(staff);
+        }
+
+        // POST: Staffs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Salary")] Staff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(staff).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Id = new SelectList(db.Teachers, "StaffId", "Specialty", staff.Id);
+            return View(staff);
+        }
+
+        // GET: Staffs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Staff staff = db.Staff.Find(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            return View(staff);
+        }
+
+        // POST: Staffs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Staff staff = db.Staff.Find(id);
+            db.Staff.Remove(staff);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
